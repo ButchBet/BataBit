e7474d600612c7a5696fb5869946d553e2bdcc36
ButchBet <butchbet01@gmail.com>
2022-08-17 13:08:01 -0500

    Changed some values of the style and completed the script.

--- js/script.js
+++ js/script.js
@@ -3,11 +3,19 @@
 // Script to change the coins to the commissions
 const leftArrow = document.getElementById("left-arrow");
 const rightArrow = document.getElementById("right-arrow");
+const coinsCard = document.getElementById("coins");
+const commissionsCard = document.getElementById("commissions");
 
 leftArrow.addEventListener("click", (e) => {
-    alert("Hello World");
+    coinsCard.classList.remove("hidden");
+    commissionsCard.classList.add("hidden");
+    leftArrow.classList.add("hidden");
+    rightArrow.classList.remove("hidden");
 });
 
 rightArrow.addEventListener("click", (e) => {
-    alert("Hello World 2");
-})
+    coinsCard.classList.add("hidden");
+    commissionsCard.classList.remove("hidden");
+    rightArrow.classList.add("hidden");
+    leftArrow.classList.remove("hidden");
+});
