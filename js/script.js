c47638ea8c00dc97e1dc275864c11bba696ed2d2
ButchBet <butchbet01@gmail.com>
2022-08-16 23:33:05 -0500

    Begun the creationg of an script to change the coins to the commissions.

---
+++ js/script.js
@@ -0,0 +1,13 @@
+"use strict"
+
+// Script to change the coins to the commissions
+const leftArrow = document.getElementById("left-arrow");
+const rightArrow = document.getElementById("right-arrow");
+
+leftArrow.addEventListener("click", (e) => {
+    alert("Hello World");
+});
+
+rightArrow.addEventListener("click", (e) => {
+    alert("Hello World 2");
+})
