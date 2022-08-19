574e9b3a00fdf746863a565635b51512d45d6729
ButchBet <butchbet01@gmail.com>
2022-08-18 22:59:00 -0500

    Script move through the plans panel.

--- js/script.js
+++ js/script.js
@@ -25,3 +25,68 @@ const footerYear = document.getElementById("year");
 const year = new Date().getFullYear();
 
 footerYear.innerHTML= year.toString();
+
+// Script move throw the plans panel
+const firstPlan = document.getElementById("first-plan");
+const secondPlan = document.getElementById("second-plan");
+const thirdPlan = document.getElementById("third-plan");
+
+const plansLeftArrow = document.getElementById("plans-left-arrow");
+const plansRightArrow = document.getElementById("plans-right-arrow");
+
+let position = 1;
+// 0 = firstPlan
+// 1 = secondPlan
+// 2 = thirdPlan
+// console.log(firstPlan, secondPlan, thirdPlan, plansLeftArrow, plansRightArrow);
+
+plansLeftArrow.addEventListener("click", (e) => {
+    switch(position) {
+        case 0:
+            firstPlan.classList.add("hidden");
+
+            thirdPlan.classList.remove("hidden");
+
+            position = 2;
+            break;
+        case 1:
+            secondPlan.classList.add("hidden");
+
+            firstPlan.classList.remove("hidden");
+
+            position = 0;
+            break;
+        default:
+            thirdPlan.classList.add("hidden");
+
+            secondPlan.classList.remove("hidden");
+
+            position = 1;
+    }
+});
+
+
+plansRightArrow.addEventListener("click", (e) => {
+    switch(position) {
+        case 0:
+            firstPlan.classList.add("hidden");
+
+            secondPlan.classList.remove("hidden");
+
+            position = 1;
+            break;
+        case 1: 
+            secondPlan.classList.add("hidden");
+
+            thirdPlan.classList.remove("hidden");
+
+            position = 2;
+            break;
+        default:
+            thirdPlan.classList.add("hidden");
+
+            firstPlan.classList.remove("hidden");
+
+            position = 0;
+    }
+});
